452cbc11573e06bfc3945e507223498df277bc18
CalinNavadaru <calin.navadaru@stud.ubbcluj.ro>
2024-05-24 11:33:15 +0300

    Easy problems
    LinkedListCycle
    MajorityElement
    AddDigits
    AddStrings

--- python/169_MajorityElement.py
+++ python/169_MajorityElement.py
@@ -3,13 +3,14 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        majority_element = nums[0]
+        majority_number = 0
         freq = 0
-        for num in nums:
+        for elem in nums:
             if freq == 0:
-                majority_element = num
-            if majority_element == num:
+                majority_number = elem
+            if majority_number == elem:
                 freq += 1
             else:
                 freq -= 1
-        return majority_element
+
+        return majority_number
