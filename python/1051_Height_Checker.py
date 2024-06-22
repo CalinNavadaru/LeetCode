0216c8a30bdd293eb86a854f1bf1c3916ee1d52a
CalinNavadaru <calin.navadaru@stud.ubbcluj.ro>
2024-06-22 16:39:21 +0300

    Some problems
    2619
    2703
    1051
    344
    9

---
+++ python/1051_Height_Checker.py
@@ -0,0 +1,4 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        correct_heights = list(sorted(heights))
+        return sum(x != y for x, y in zip(correct_heights, heights))
